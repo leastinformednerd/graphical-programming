496ec0630e267d660805a16b4104f6cb517c6f3a
leastinformednerd <automagicwyvern@gmail.com>
2024-06-19 22:39:58 +1200

    Hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    routing::get,
+    Router,
+    response::Html
+};
+
+async fn index() -> Html<String>{
+    Html("Hello World!".into())
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(index));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
